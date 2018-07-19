551dcb4c79c2415fdb5c11d81ea2d74188d79a5a
Severin Parkhomenko <sparkhomenko@grossum.com>
2018-07-19 10:29:47 +0300

    add bmp resizing method

---
+++ app/src/main/java/com/theappsolutions/boilerplate/util/other/BitmapUtils.java
@@ -0,0 +1,22 @@
+package com.theappsolutions.boilerplate.util.other;
+
+import android.graphics.Bitmap;
+
+public class BitmapUtils {
+
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+}
